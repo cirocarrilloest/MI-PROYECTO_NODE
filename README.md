750eb8b0541dd1039951cd51b359760850d0b946
cirocarrilloest <ciro_carrillo@soy.sena.edu.co>
2026-04-09 18:50:44 -0500

    primera carga

---
+++ README.md
@@ -0,0 +1,12 @@
+##instalacion
+
+- npm init -y
+- npm i express mongoose bcryptjs jsonwebtoken dotenv cors
+- npm i mysql2 joi
+- npm i -D nodemon
+- npm install morgan
+- npm install chalk
+- npm install clear
+
+##ejecutar aplicacion
+npm run dev
---
+++ db.sql
@@ -0,0 +1,19 @@
+-- creamos base de datos
+create database if not exists validacion_db;
+use validacion_db;
+-- creamos tabla de pruebas
+create table if not exists usuarios(
+	id int primary key auto_increment,
+    nombre varchar(100) not null,
+    email varchar(100) unique not null,
+    pass varchar(255) not null,
+    rol enum('user','admin') default 'user',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    INDEX idx_email(email)
+); 
+-- vemos detalle de la tabla
+describe usuarios;
+
+-- vemos datos
+select * from usuarios;
